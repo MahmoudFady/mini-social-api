b31924bde5ac0d7eae18e6c1ed43786be1f5f684
Mahmoud_Fady <58427542+MahmoudFady@users.noreply.github.com>
2022-11-26 16:48:10 +0200

    enhance all cases using try & catch

--- app/controller/user.js
+++ app/controller/user.js
@@ -1,50 +1,60 @@
 const User = require("../model/user");
 const bcrypt = require("bcrypt");
 module.exports.signup = async (req, res, next) => {
-  const { fullName, email, phoneNumber, address, birthDate, password } =
-    req.body;
-  const { country, state, city } = address;
-  const existedUser = await User.findOne({
-    $or: [{ email }, { phoneNumber }],
-  });
-  if (existedUser) {
-    const message =
-      existedUser.email == email ? "email address" : "phone number";
-    return res
-      .status(409)
-      .json({ message: message + " already exist", user: null });
+  try {
+    const { fullName, email, phoneNumber, address, birthDate, password } =
+      req.body;
+    const { country, state, city } = address;
+    const existedUser = await User.findOne({
+      $or: [{ email }, { phoneNumber }],
+    });
+    if (existedUser) {
+      const message =
+        existedUser.email == email ? "email address" : "phone number";
+      return res
+        .status(409)
+        .json({ message: message + " already exist", user: null });
+    }
+    const hash = await bcrypt.hash(password, 10);
+    const newUser = await new User({
+      fullName,
+      email,
+      phoneNumber,
+      address: { country, state, city },
+      birthDate: new Date(birthDate),
+      password: hash,
+    }).save();
+    return res.status(201).json({
+      message: "user signed up",
+      user: newUser,
+    });
+  } catch (err) {
+    res.status(500).json({ message: "something go wrong" });
   }
-  const hash = await bcrypt.hash(password, 10);
-  const newUser = await new User({
-    fullName,
-    email,
-    phoneNumber,
-    address: { country, state, city },
-    birthDate: new Date(birthDate),
-    password: hash,
-  }).save();
-  return res.status(201).json({
-    message: "user signed up",
-    user: newUser,
-  });
 };
 
 module.exports.singin = async (req, res, next) => {
-  const { email, phoneNumber, password } = req.body;
-  const criteria = email ? { email } : { phoneNumber };
-  const existedUser = await User.findOne(criteria);
-  if (!existedUser) {
+  try {
+    const { email, phoneNumber, password } = req.body;
+    const criteria = email ? { email } : { phoneNumber };
+    const existedUser = await User.findOne(criteria);
+    console.log(existedUser);
+    if (!existedUser) {
+      console.log("user does not exist");
+      return res
+        .status(404)
+        .json({ message: "account does't exist", user: null });
+    }
+    const isPasswordSame = await bcrypt.compare(password, existedUser.password);
+    if (!isPasswordSame) {
+      return res.status(401).json({ message: "wrong password", user: null });
+    }
     return res
-      .status(204)
-      .json({ message: "account does't exisit", user: null });
+      .status(200)
+      .json({ message: "successfully signin", user: existedUser });
+  } catch (err) {
+    res.status(500).json({ message: "something go wrong" });
   }
-  const isPasswordSame = await bcrypt.compare(password, existedUser.password);
-  if (!isPasswordSame) {
-    return res.status(401).json({ message: "wrong password", user: null });
-  }
-  return res
-    .status(200)
-    .json({ message: "successfully signin", user: existedUser });
 };
 module.exports.update = (req, res, next) => {
   res.status(200).json({ message: "update route" });
